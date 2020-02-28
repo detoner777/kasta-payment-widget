4dc16d96f452cbe24cc72d747d1191d8363731fa
detoner <detoner777@gmail.com>
2020-02-28 10:42:06 +0200

    Updates

---
+++ precache-manifest.2ec2f02052ec3cbb8f3a7b45a9f02ebd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200def3313a671bc27087a2a6c748d40",
+    "url": "/kasta-payment-widget/index.html"
+  },
+  {
+    "revision": "51f615e2b89f4090b98b",
+    "url": "/kasta-payment-widget/static/css/main.8d174728.chunk.css"
+  },
+  {
+    "revision": "ab93d7f24127adbb7b0a",
+    "url": "/kasta-payment-widget/static/js/2.05893fb9.chunk.js"
+  },
+  {
+    "revision": "eba08dcb983c249067ba1a522c73e1ee",
+    "url": "/kasta-payment-widget/static/js/2.05893fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f615e2b89f4090b98b",
+    "url": "/kasta-payment-widget/static/js/main.f04666de.chunk.js"
+  },
+  {
+    "revision": "767d0e399c4393f44bca",
+    "url": "/kasta-payment-widget/static/js/runtime-main.d6b3b37e.js"
+  },
+  {
+    "revision": "505be85df31553a7242fb1de85acaf4c",
+    "url": "/kasta-payment-widget/static/media/credit-card.505be85d.svg"
+  },
+  {
+    "revision": "2178f575ae9be8e1e9b1d640a5aedab2",
+    "url": "/kasta-payment-widget/static/media/dss-logo.2178f575.svg"
+  },
+  {
+    "revision": "29565a74573ae83d4f7f5c42f1a43177",
+    "url": "/kasta-payment-widget/static/media/kasta-logo.29565a74.svg"
+  },
+  {
+    "revision": "b14b5b8cb86daf8415c7211334321f25",
+    "url": "/kasta-payment-widget/static/media/kastapay.b14b5b8c.svg"
+  },
+  {
+    "revision": "4fb148fefc024a4daf165d7d6f6cc37f",
+    "url": "/kasta-payment-widget/static/media/mastercard-secure.4fb148fe.svg"
+  },
+  {
+    "revision": "0a607ec2b1805d6ab361e309ca53a5c9",
+    "url": "/kasta-payment-widget/static/media/question-mark.0a607ec2.svg"
+  },
+  {
+    "revision": "b9ba880a88ad009e0e578f9d5529242a",
+    "url": "/kasta-payment-widget/static/media/visa-secure.b9ba880a.svg"
+  }
+]);
